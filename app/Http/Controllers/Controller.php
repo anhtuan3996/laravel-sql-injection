96c87547511fa68fcc863a779faf96ab9149db40
Tuan CA <tuan.ca@neo-lab.vn>
2020-02-04 10:59:18 +0700

    test sql injection

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * ./sqlmap.py -u http://localhost:8000/sql-injection\?sort\=name --dbms=mysql --current-db --current-user --users
+     */
+    public function sqlInjection(Request $request)
+    {
+        $order= $request->input('sort', 'id');
+//        $users = User::all();
+        $users = DB::select("select * from users order by $order desc");
+
+        return response()->json($users);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/sql-injection', 'Controller@sqlInjection');
